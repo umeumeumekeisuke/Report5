6251321dfb5d5fa08cd7208f40af61301e9cf522
梅田敬祐 <umedatakashiyuu@umedatakashiyuunoMacBook-Air.local>
2023-01-17 20:40:58 +0900

    report3

--- src/Report5.java
+++ src/Report5.java
@@ -1,12 +1,6 @@
 public class Report5{
     public static void main(String[] args){
-        try{
-            String str = null;
-            System.out.println(str.length());
-        }
-        catch(NullPointerException e){
-            System.out.println("NullPointerExceptionエラー発生");
-            System.out.print(e.getMessage());
-        }
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
     }
 }
