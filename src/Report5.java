3edc234443e1c88e2f6506c763a522f1022fe3b8
梅田敬祐 <umedatakashiyuu@umedatakashiyuunoMacBook-Air.local>
2023-01-17 20:37:14 +0900

    step2

--- src/Report5.java
+++ src/Report5.java
@@ -1,6 +1,12 @@
-public class Report5 {
+public class Report5{
     public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+        try{
+            String str = null;
+            System.out.println(str.length());
+        }
+        catch(NullPointerException e){
+            System.out.println("NullPointerExceptionエラー発生");
+            System.out.print(e.getMessage());
+        }
     }
 }
